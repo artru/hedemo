4eb4e0f8a76f359c3f79a19eaa5088b8a69ffbf4
artru <ravendv@lst.ru>
2012-05-30 11:56:31 +0300

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hedemo::Application.config.secret_token = '94bd3d8bf9d997d3b9b94255b3e32cb738048771f1aa5e035b5290d75144eba733ef892a1bd8a233efbc73967e67b35d690e19b8f1257ff2f3fc666432e48f4d'
